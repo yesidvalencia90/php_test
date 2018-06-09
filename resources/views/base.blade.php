cbd49ac38dcf10a8675a74b3318114c7cf4c7f54
yesid.valencia <yesid.valencia@paradisosolutions.com>
2018-06-08 23:48:09 -0500

    Auth user routes segregation

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -29,17 +29,33 @@
 	    <!-- Collect the nav links, forms, and other content for toggling -->
 	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 	      <ul class="nav navbar-nav">
-	        <li><a href="{{ url('/users') }}">Users <span class="sr-only">(current)</span></a></li>
-	        <li><a href="{{ url('/buses') }}">Buses</a></li>	 
-	        <li><a href="{{ url('/routes') }}">Routes</a></li>	 
+
+	      	 @if(Auth::user())
+
+		        <li><a href="{{ url('/users') }}">Users <span class="sr-only">(current)</span></a></li>
+		        <li><a href="{{ url('/buses') }}">Buses</a></li>	 
+		        <li><a href="{{ url('/routes') }}">Routes</a></li>	
+
+	        @endif
+
 	        <li><a href="{{ url('/quote') }}">Price Quote</a></li>	 
 	      </ul>
 	      <ul class="nav navbar-nav navbar-right">
 	        <li>
-	        	<a href="#" id="logout_button">Logout</a>
-	        	<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                    @csrf
-                </form>
+
+	        	 @if(Auth::user())
+
+		        	<a href="#" id="logout_button">{{ Auth::user()->name }} (Logout)</a>
+		        	<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+	                    @csrf
+	                </form>
+
+	             @else
+
+                	<a href="{{ asset('/login') }}" >Login</a>
+
+	             @endif
+
 	        </li>
 	      </ul>
 	    </div><!-- /.navbar-collapse -->
